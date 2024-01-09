c6c620ef39686622df85fe7f7b351d5d74ca03d8
Kris <ssspers92@gmail.com>
2024-01-09 19:41:59 +0300

    delete trash and install python 3.7.8 for ONE library

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,7 @@
-
-
+from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
+    youtube = build()
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
