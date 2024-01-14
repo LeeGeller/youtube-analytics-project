9505339107a65b5f8528c6ccd50780e4f2ae080b
Kris <ssspers92@gmail.com>
2024-01-14 13:54:12 +0300

    create get_service and params title, video_count and url

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,6 @@
-from googleapiclient.discovery import build
+import os
 
-from config import YOU_TUBE_API_KEY
+from googleapiclient.discovery import build
 
 
 class Channel:
@@ -9,9 +9,38 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.title = ''
+        self.video_count = ''
+        self.url = ''
+        self.all_info = None
+
+    def __repr__(self):
+        return (f"{self.channel_id} id канала\n"
+                f"{self.title} название канала\n"
+                f"{self.video_count} количество видео\n"
+                f"{self.url} url")
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         youtube = build('youtube', 'v3', developerKey="AIzaSyBh6pGPuYR5rbNQDIsLLqktyXtNTJWGHWA")
-        channel_info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(channel_info)
+        self.all_info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+        for key, value in self.all_info.items():
+            if key == 'items':
+                for items in value:
+                    self.title = items['snippet']['title']
+                    self.video_count = items["statistics"]["videoCount"]
+                    self.url = items["kind"]
+
+        return self.all_info
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey="AIzaSyBh6pGPuYR5rbNQDIsLLqktyXtNTJWGHWA")
+
+
+moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+print(moscowpython.print_info())
+print(moscowpython.all_info)
+print(moscowpython.title)
+print(moscowpython.video_count)
