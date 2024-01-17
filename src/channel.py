dcff90e7091cc813f00dc453605ffc279e0425e0
Kris <ssspers92@gmail.com>
2024-01-17 17:10:29 +0300

    create magic metods: __str__, __add__, __sub__, __lt__, __le__, __gt__, __ge__, __eq__

--- src/channel.py
+++ src/channel.py
@@ -19,9 +19,33 @@ class Channel:
                 f"{self.video_count} количество видео\n"
                 f"{self.url} url")
 
+    def __str__(self):
+        return f'{self.title} {self.url}'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
+
     def get_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey="AIzaSyBh6pGPuYR5rbNQDIsLLqktyXtNTJWGHWA")
+        youtube = build('youtube', 'v3', developerKey=os.getenv("TEST_YOUTUBE_API"))
         return youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
 
     def print_info(self):
